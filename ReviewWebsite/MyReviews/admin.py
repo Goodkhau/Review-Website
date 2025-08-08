797e2631b5575a746ad99a81c09a0f7ec2647277
Goodkhau <gos.khai12@gmail.com>
2025-08-07 21:53:01 -0400

    Registered models to admin site

--- ReviewWebsite/MyReviews/admin.py
+++ ReviewWebsite/MyReviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Genre, Movie, User, Review
+
+admin.site.register(User)
+admin.site.register(Review)
+admin.site.register(Movie)
+admin.site.register(Genre)
