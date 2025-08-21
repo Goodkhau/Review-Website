27dfe464b647c145ba9d70ab5b07fa3bd906e767
Goodkhau <gos.khai12@gmail.com>
2025-08-20 21:00:24 -0400

    Created userform

---
+++ ReviewWebsite/MyReviews/userform.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import User
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = {'email', 'username', 'password', 'confirmpass'}
--- ReviewWebsite/MyReviews/userviews.py
+++ ReviewWebsite/MyReviews/userviews.py
@@ -1,3 +1,5 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .models import User
 
