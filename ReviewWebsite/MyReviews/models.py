6ea021c94900fd4a2bce8009992847e03bc4684c
Goodkhau <gos.khai12@gmail.com>
2025-08-25 21:24:04 -0400

    Added imagefield to person and user. Also added average score to movie

--- ReviewWebsite/MyReviews/models.py
+++ ReviewWebsite/MyReviews/models.py
@@ -12,12 +12,14 @@ class Person(models.Model):
     birth = models.DateField(blank=True)
     death = models.DateField(blank=True)
     biography = models.TextField(blank=True)
+    picture = models.ImageField(default='fallback.png', blank=True)
 
 class Movie(models.Model):
     movie_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     poster = models.ImageField(default='fallback.png', blank=True)
+    average_score = models.DecimalField(max_digits=4, decimal_places=2, null=True)
     total_score = models.IntegerField(default=0)
     number_reviews = models.IntegerField(default=0)
     genre_list = models.ManyToManyField(Genre, blank=True)
@@ -50,6 +52,7 @@ class User(AbstractBaseUser):
     username = models.CharField(max_length=40, unique=True)
     email = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    picture = models.ImageField(default='fallback.png', blank=True)
     # verification_token
     # verification_expire
     # verification_date
