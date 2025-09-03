c3288ff944db9a05824d21435dd60d706d0a2348
Goodkhau <gos.khai12@gmail.com>
2025-09-02 23:55:35 -0400

    Revert "Created main function"
    This reverts commit c4a54c00a91362d6fcb8a9285f6b3f22b6279a72.

--- ReviewWebsite/import.py
+++ ReviewWebsite/import.py
@@ -2,4 +2,15 @@ import MySQLdb
 from decouple import config
 
 def main():
+    print("Select an option.")
+    print("1. Import movie data")
+    selection = 0
+    while selection == 0:
+        try:
+            selection = int(input())
+        except ValueError:
+            print("Invalid option.")
     
+    match selection:
+        case 1:
+            
