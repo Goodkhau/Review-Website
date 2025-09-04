e3ac183dc9c74841d21c8a30af71800deb6dc997
Goodkhau <gos.khai12@gmail.com>
2025-09-03 23:42:40 -0400

    Updating import.py

--- ReviewWebsite/import.py
+++ ReviewWebsite/import.py
@@ -1,6 +1,9 @@
 import MySQLdb
 from decouple import config
 
+db = MySQLdb.connect(host=config('HOST'), user=config('DB_USER'), 
+    password=config('DB_PASS'), database=config('DATABASE'))
+
 def main():
     print("Select an option.")
     print("1. Import movie data")
@@ -13,4 +16,19 @@ def main():
     
     match selection:
         case 1:
-            
+            ImportMovies()
+
+def ImportMovies():
+    has_file = False
+    while not has_file:
+        try:
+            directory = input()
+            data = open(directory+"/movies_metadata.csv", 'r')
+            has_file = True
+        except FileNotFoundError:
+            print("This directory does not exist")
+    
+    debug = input("Would you like to see the data? (Y/N)")
+    if debug == "Y":
+        for line in data:
+            print(line)
