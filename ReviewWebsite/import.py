77804fd05dda5d0eb3c6f6974fc243a016e7d496
Goodkhau <gos.khai12@gmail.com>
2025-09-06 01:55:45 -0400

    Added try catch statements and completed querying most movies. Implement Genre and cast and crew next.

--- ReviewWebsite/import.py
+++ ReviewWebsite/import.py
@@ -1,9 +1,11 @@
 import MySQLdb
 import json
 from decouple import config
+from datetime import datetime
 
 db = MySQLdb.connect(host=config('HOST'), user=config('DB_USER'), 
     password=config('DB_PASS'), database=config('DATABASE'))
+cur = db.cursor()
 
 
 def main():
@@ -31,17 +33,21 @@ def ImportMovies():
         except FileNotFoundError:
             print("This directory does not exist")
     
-    debug = input("Would you like to see the data? (Y/N)")
+    debug = input("Would you like to see the data? (Y/N)\n")
     if debug == "Y":
         i = 0
         for line in data:
             print(line)
+            i+=1
+            if i >= 10:
+                break
     
     split = input("Decode the data? (Y/N)\n")
     rows = []
     if (split == "Y"):
         for line in data:
             rows.append(parseline(line))
+        rows.pop(0)
         
         check = input("Check decoded data? (Y/N)\n")
         if (check == "Y"):
@@ -57,8 +63,24 @@ def ImportMovies():
 
     insert = input("Insert the data? (Y/N)\n")
     if insert == "Y":
-        for line in data:
-            print(line)
+        error_count = 0
+        accepted_count = 0
+        for row in rows:
+            try:
+                if row[16] == '':
+                    row[16] = 0
+                row[14] = datetime.strptime(row[14], '%Y-%m-%d').date()
+                cur.execute("INSERT INTO myreviews_movie (movie_id, title, description, release_date, runtime, " \
+                "date_added, number_reviews, poster, total_score, director_id, average_score) " \
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (row[5], row[8], row[9], row[14], row[16], datetime.now(), 0, "fallback.png", 0, None, None)
+                )
+                accepted_count+=1
+            except:
+                error_count+=1
+                print("Error for: ", end="")
+                print(row)
+        print("There were " + str(error_count) + " errors and " + str(accepted_count) + " accepted.")
 
 def parseline(line):
     row = []
@@ -71,11 +93,16 @@ def parseline(line):
         if (line[i] == '"'):
             i+=1
             L=i
-            while line[i] != '"':
-                i+=1
-                if i >= len(line):
-                    i-=1
-                    break
+            try:
+                while line[i] != '"' or line[i+1] != ',' or line[i-1] == '"':
+                    i+=1
+                    if i >= len(line):
+                        i-=1
+                        break
+            except IndexError:
+                print("Error for (likely newlines in the description):")
+                print(line)
+                SystemExit
             row.append(line[L:i])
             i+=1
             L=i+1
