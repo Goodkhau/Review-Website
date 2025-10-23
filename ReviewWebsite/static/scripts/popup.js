20cd339a40f47aca22e7eb9c05deb3623af804de
Goodkhau <gos.khai12@gmail.com>
2025-10-22 23:32:16 -0400

    Updated popup.js to have functions for the other popups and stylized the three popups to sit on top of the page but below the nav bar.

--- ReviewWebsite/static/scripts/popup.js
+++ ReviewWebsite/static/scripts/popup.js
@@ -1,4 +1,18 @@
 function displayReviewForm() {
-    document.getElementById("review-popup").style.display = "block"
-    print("Executed\n")
+    document.getElementById("review-popup").style.visibility = "visible"
+}
+function displayMovieForm() {
+    document.getElementById("movie-popup").style.visibility = "visible"
+}
+function displayUserForm() {
+    document.getElementById("user-popup").style.visibility = "visible"
+}
+function closeReviewForm() {
+    document.getElementById("review-popup").style.visibility = "hidden"
+}
+function closeMovieForm() {
+    document.getElementById("movie-popup").style.visibility = "hidden"
+}
+function closeUserForm() {
+    document.getElementById("user-popup").style.visibility = "hidden"
 }
